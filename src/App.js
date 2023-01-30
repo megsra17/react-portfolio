ae379430873e0158d90eb2059c840c5472f0b0de
Megan Rakow <meganrakow@Megans-MacBook-Pro-2.local>
2023-01-29 18:45:55 -0600

    added project section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skill from "./components/Skills";
+import Project from "./components/Projects";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skill />
+      <Project />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,47 @@
+const Project = () => {
+  const projects = [
+    {
+      title: "SurfTaco",
+      description: "Resturant Located throughout New Jersey",
+      imgUrl: projImg1,
+      url: "https://surftaco.com/",
+    },
+    {
+      title: "A2Z Diagnostics",
+      description:
+        "A2Z Diagnostics is a leading-edge CAP-accredited, CLIA-certifed lab providing state-of-the-art pathology, with results",
+      imgUrl: projImg2,
+      url: "https://www.a2zdx.com/",
+    },
+    {
+      title: "dan gentile dog training",
+      description:
+        "Dan Gentile's Dog Training Center, where we've been training dogs for over 40 years.",
+      imgUrl: projImg3,
+      url: "https://njdog.com/",
+    },
+    {
+      title: "The Laundry Place",
+      description:
+        "We are the FASTEST way to get laundry done! Our clean and inviting environment offers plenty of seating, folding tables, and entertainment options.",
+      imgUrl: projImg1,
+      url: "https://thelaundryplace.com/",
+    },
+    {
+      title: "Hudson Speed",
+      description:
+        "HUDSON SPEED helps athletes improve their athleticism and endurance so they can have the best sports performance",
+      imgUrl: projImg2,
+      url: "https://hudsonspeed.com/",
+    },
+    {
+      title: "Movie Soundtrack Finder",
+      description: "Helps you find Playlist on Spotify from movies",
+      imgUrl: projImg3,
+      url: "https://megsra17.github.io/movie-soundtrack-finder/",
+    },
+  ];
+  return "hello";
+};
+
+export default Project;
